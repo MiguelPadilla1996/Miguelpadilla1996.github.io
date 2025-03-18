d75541de572a1776f6e99ccac37615b5c47c6e76
Hero9696 <Miguelpadilla9619@gmail.com>
2025-03-18 15:28:45 -0600

    update pages

--- web/views/modules/headerComponent.js
+++ web/views/modules/headerComponent.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
         <nav class="navegacion">
             <ul class="menu">
                 <li><a href="/index.html">Inicio</a></li>
-                <li><a href="/sobremi.html">Sobre Mi</a></li>
+                <li><a href="/web/views/pages/sobremi/sobremi.html">Sobre Mi</a></li>
                 <li><a href="/web/views/pages/contacto/contacto.html">Contacto</a></li>
                 <li><a href="#">Proyectos</a>
                     <ul class="submenu">
                         <li><a href="/web/views/pages/encriptador/encriptador.html" >Encriptador de Mensajes</a></li>
                         <li><a href="/web/views/pages/ahorcado/ahorcado.html" target="_blank">Juego del Ahorcado</a></li>
-                        <li><a href="/tienda.html" target="_blank">Tienda Online</a></li>
+                        <li><a href="/web/views/pages/tienda/tienda.html" target="_blank">Tienda Online</a></li>
                         <li><a href="/web/views/pages/carrito/carrito.html">Carrito De Compras</a></li>
                     </ul>
                 </li>
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <li><a href="#"></a></li>
                 </ul></li>
                 <li><a href="/web/views/pages/certificaciones/certificaciones.html">Mis Certificaciones</a></li>
-                <li><a href="/paginaestatica.html">Mi Pagina Estatica</a></li>
+                <li><a href="/web/views/pages/paginaestatica/paginaestatica.html">Mi Pagina Estatica</a></li>
             </ul>
         </nav>
     </header>
