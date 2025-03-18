3d71dee92e5bb0de7f5579a51c01f38d64833c8d
Hero9696 <Miguelpadilla9619@gmail.com>
2025-03-18 14:35:05 -0600

    updates pages

--- web/views/modules/footerComponent.js
+++ web/views/modules/footerComponent.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("footer").innerHTML = `
         <footer class="footer" >
-    <p>&#9400 Derechos Reservados Miguel Angel Padilla Morales 2022 </p>
+    <p>&#9400 Derechos Reservados Miguel Angel Padilla Morales 2025 </p>
 </footer>
     `;
 });
--- web/views/modules/headerComponent.js
+++ web/views/modules/headerComponent.js
@@ -5,20 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
             <ul class="menu">
                 <li><a href="/index.html">Inicio</a></li>
                 <li><a href="/sobremi.html">Sobre Mi</a></li>
-                <li><a href="/contacto.html">Contacto</a></li>
+                <li><a href="/web/views/pages/contacto/contacto.html">Contacto</a></li>
                 <li><a href="#">Proyectos</a>
                     <ul class="submenu">
-                        <li><a href="/encriptador.html" target="_blank">Encriptador de Mensajes</a></li>
-                        <li><a href="/ahorcado.html" target="_blank">Juego del Ahorcado</a></li>
+                        <li><a href="/web/views/pages/encriptador/encriptador.html" >Encriptador de Mensajes</a></li>
+                        <li><a href="/web/views/pages/ahorcado/ahorcado.html" target="_blank">Juego del Ahorcado</a></li>
                         <li><a href="/tienda.html" target="_blank">Tienda Online</a></li>
+                        <li><a href="/web/views/pages/carrito/carrito.html">Carrito De Compras</a></li>
                     </ul>
                 </li>
                 <li><a href="#">Juegos</a>
                 <ul class="submenu">
-                    <li><a href="/acertar.html" >Tiro al Blanco</a></li>
+                    <li><a href="/web/views/pages/acertar/acertar.html" >Tiro al Blanco</a></li>
                     <li><a href="#"></a></li>
                 </ul></li>
-                <li><a href="/certificaciones.html">Mis Certificaciones</a></li>
+                <li><a href="/web/views/pages/certificaciones/certificaciones.html">Mis Certificaciones</a></li>
                 <li><a href="/paginaestatica.html">Mi Pagina Estatica</a></li>
             </ul>
         </nav>
